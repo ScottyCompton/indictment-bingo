c616fe4bbba77f73d5ffe2532ff7a1aafc11e976
Scott Lonis <scott.lonis@outlook.com>
2021-07-18 22:16:29 -0600

    added subject images to app for faster card generator rendering

--- src/helpers/index.tsx
+++ src/helpers/index.tsx
@@ -105,7 +105,7 @@ export const putData = async (endpoint:string, cfg:PutDataConfiguration) => {
 export const appConfig:AppConfig = {
         rollImgRoot: process.env.REACT_APP_ROLLERS_IMG_ROOT!,
         audioRoot: process.env.REACT_APP_AUDIO_ROOT!,
-        subjImgRoot: process.env.REACT_APP_API_ROOT + process.env.REACT_APP_SUBJECT_IMG_ROOT!,
+        subjImgRoot: process.env.REACT_APP_SUBJECT_IMG_ROOT!,
         cardThumbImgRoot: process.env.REACT_APP_API_ROOT + process.env.REACT_APP_THUMB_IMG_ROOT!,
         playLength: 18000,
         apiRoot: process.env.REACT_APP_API_ROOT!,
